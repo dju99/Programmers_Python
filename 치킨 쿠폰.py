626a64cb05c669f10dccc246de6438bb3c4063ba
dju99 <137643698+dju99@users.noreply.github.com>
2023-06-27 02:29:42 +0900

    Add files via upload

---
+++ 치킨 쿠폰.py
@@ -0,0 +1,10 @@
+def solution(chicken):
+    answer = 0
+    
+    while chicken >= 10:
+        service = chicken // 10
+        coupon = chicken % 10
+        answer += service
+        chicken = service + coupon
+        
+    return answer
